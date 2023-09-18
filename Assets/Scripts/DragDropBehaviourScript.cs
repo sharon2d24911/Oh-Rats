ff0d6462fd216f4e2b6aa2cb7ae11d4110835fee
Jill <jzzheng@ualberta.ca>
2023-09-17 22:42:42 -0600

    Improved drag & drop (transparency, restrictive drags) and incorporated with grid system

--- Assets/Scripts/DragDropBehaviourScript.cs
+++ Assets/Scripts/DragDropBehaviourScript.cs
@@ -7,16 +7,18 @@ public class DragDropBehaviourScript : MonoBehaviour
     private GameObject selectedObject;
     private Vector3 startingPosition;
     private List<GameObject> combining = new List<GameObject>();
+    private List<GameObject> dragged = new List<GameObject>();
     public GameObject combinationZone;
-    private float sensitivity = 1.0f;
+    private readonly float sensitivity = 2.0f;
+    private bool isIngredient = false;
     private GameObject grid;
 
     void Start()
     {
         grid = GameObject.Find("PlaceholderGrid");
     }
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    void Update()
     {
 
         // If left mouse button is clicked
@@ -25,21 +27,21 @@ public class DragDropBehaviourScript : MonoBehaviour
             CheckHitObject();
         }
 
-        // Dragging the selected object
+        // If left mouse button is held down
         if (Input.GetMouseButton(0) && selectedObject != null)
         {
-            // Change the position of the selected object based on the position of the mouse (accounting for offset)
-            selectedObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane + 10.0f));
+            DragObject();
         }
-        
-        // Letting go of mouse button releases selected object
+
+        // If left mouse button is released
         if (Input.GetMouseButtonUp(0) && selectedObject != null)
         {
             DropObject();
         }
-        
+
     }
 
+    // Checks if there is an object that can be selected at the mouse position
     void CheckHitObject()
     {
         // Gets mouse coordinates and maps to where it is on the game screen
@@ -47,30 +49,44 @@ public class DragDropBehaviourScript : MonoBehaviour
 
         // Raycasting: did the mouse hit a collider?
         RaycastHit2D hit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
-        if (hit.collider != null)
+        if (hit.collider != null && !dragged.Contains(hit.collider.gameObject))
         {
             selectedObject = hit.collider.gameObject;
             startingPosition = selectedObject.transform.position;
+
+            if (selectedObject.tag == "Ingredient")
+            {
+                isIngredient = true;
+                GameObject clone = Instantiate(selectedObject);
+            } else
+            {
+                isIngredient = false;
+            }
         }
     }
 
+    // Drags selected object if something is at mouse position
+    void DragObject()
+    {
+        // Set transparency of object to 70%
+        selectedObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .7f);
+
+        // Change the position of the selected object based on the position of the mouse (accounting for offset)
+        selectedObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane + 10.0f));
+    }
+
+    // Drops object that was being dragged by mouse
     void DropObject()
     {
+        // Resets object transparency
+        selectedObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
 
-        // Clones the selected object at its starting position once dropped
-        /* Ideally this clone should be happening at the start of the dragging to
-         * make more sense (player isn't always seemingly dragging out the last item),
-         * but having the code there right now allows players to drag clones out from
-         * the combination area after the original object has already been placed down.
-         * Will need to be fixed later on but should still work for the prototype logic.
-         */
-        GameObject clone = Instantiate(selectedObject);
         GridCreate gridScript = grid.GetComponent<GridCreate>();
         Vector3 nearestPos = selectedObject.transform.position;
         float nearestDistance = Vector3.Distance(grid.transform.position, selectedObject.transform.position);
         List<Vector3> gridPositions;
         gridPositions = gridScript.getPositions(); //grabs list of grid positions from the GridCreate script
-        clone.transform.position = startingPosition;
+
 
         Debug.Log(nearestDistance);
 
@@ -84,35 +100,54 @@ public class DragDropBehaviourScript : MonoBehaviour
             }
         }
 
-
         Debug.Log(nearestDistance);
         Debug.Log(nearestPos);
 
-        // Checks if the selected object is close enough or on the object that's been designated as the combination area
-        // Will only accept new object placements if the list of things to be combined is not yet at 2 (assuming 2 objects will be the limit, subject to chenge)
-        if (Vector3.Distance(combinationZone.transform.position, selectedObject.transform.position) < sensitivity && combining.Count < 2)
+        if (isIngredient)
         {
-            // Snaps object into the same position
-            selectedObject.transform.position = new Vector3(combinationZone.transform.position.x, combinationZone.transform.position.y, combinationZone.transform.position.z - 0.1f);
+            // Checks if the selected object is close enough or on the object that's been designated as the combination area
+            // Will only accept new object placements if the list of things to be combined is not yet at 2 (assuming 2 objects will be the limit, subject to chenge)
+            if (Vector3.Distance(combinationZone.transform.position, selectedObject.transform.position) < sensitivity && combining.Count < 2)
+            {
+                // Snaps object into the same position
+                selectedObject.transform.position = combinationZone.transform.position;
+                dragged.Add(selectedObject);
 
-            // Adds selected object to list
-            combining.Add(selectedObject);
-        } else if (nearestDistance < sensitivity) {
-            // Snaps object into the same position
-            selectedObject.transform.position = nearestPos;
+                // Adds selected object to list
+                combining.Add(selectedObject);
+            }
+            else
+            {
+                // Destroy the object selected if there are already 2 items in combination area or if not placed close enough
+
+                /* Should be updated by combination logic so that both items
+                 * will be destroyed either way once there are two items here
+                 * regardless of if they are allowed to combine or not.
+                 * Might need a new function, probably also an elif for when
+                 * combining.Count == 2.
+                 */
+                Destroy(selectedObject);
+            }
+        }
+        else if (nearestDistance > sensitivity)
+        {
+            // If tower is not within distance, place back in original spot and destroy current instance
+            GameObject clone = Instantiate(selectedObject);
+            clone.transform.position = startingPosition;
+            Destroy(selectedObject);
         }
         else
         {
-            // Destroy the object selected if there are already 2 items in combination area or if not placed close enough
-
-            /* Should be updated by combination logic so that both items
-             * will be destroyed either way once there are two items here
-             * regardless of if they are allowed to combine or not.
-             * Might need a new function, probably also an elif for when
-             * combining.Count == 2.
-             */
-            Destroy(selectedObject);
+            // If tower is within distance of a grid spot, snaps object into the same position
+            selectedObject.transform.position = new Vector3(nearestPos.x, nearestPos.y, nearestPos.z - 1f);
+            dragged.Add(selectedObject);
         }
+
+        /* else
+        {
+            throw new MissingComponentException("No grid detected.");
+        }
+        */
 
         selectedObject = null;
     }
