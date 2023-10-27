37042053e0bab3c9b39002f3362aa39b8e9fd2f0
Jill <jzzheng@ualberta.ca>
2023-10-27 00:03:44 -0600

    Basic interactive tutorial system set up in a new scene, edits to props in terms of sizing and click implementation (please be careful with Game.Unity scene T_T)

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,20 +5,19 @@ public class Menu : MonoBehaviour
 {
     public Animator animator;
     private string sceneToLoad;
+    public Texture2D basic;
 
     void Start()
     {
-        Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.SetCursor(basic, Vector2.zero, CursorMode.ForceSoftware);
     }
 
     // Using animator to fade scene out to black
     public void FadeToScene(string sceneName)
     {
-        
+        Time.timeScale = 1f;
         sceneToLoad = sceneName;
         animator.SetTrigger("FadeOut");
-        Camera.main.GetComponent<AudioManager>().PlaySFX("StartButton");
-        StartCoroutine(Camera.main.GetComponent<AudioManager>().Fade(true, "BassyMain", 1f, 1f)); // Fade in music
     }
 
     // Animation event on the completion of fading out to call scene
@@ -26,5 +25,4 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(sceneToLoad);
     }
-    
 }
