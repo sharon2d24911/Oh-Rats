40e10b7474290c010385bb5b8afb20bd2d9319bf
Matthieu-Larochelle <mrlaroch@ualberta.ca>
2023-12-13 20:34:14 -0700

    Manual merge with Sharon's branch and Waves updates
    Updates waves txt file being used and changed unit stats
    Manually merged with Sharon's push from 2 day ago (likely has bugs)

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class PauseMenu : MonoBehaviour
     public GameObject notification;
     [HideInInspector] public bool paused = false;
     private bool prop = false;
+    public Slider musicSlider;
+    public Slider sfxSlider;
 
     public void Pause()
     {
@@ -20,7 +23,8 @@ public class PauseMenu : MonoBehaviour
         else
             prop = false;
         Time.timeScale = 0f;
-        GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>().PlaySFX("UIClick",GameObject.FindWithTag("GameHandler").GetComponent<ReadSfxFile>().sfxDictionary["UIClick"][0], GameObject.FindWithTag("GameHandler").GetComponent<ReadSfxFile>().sfxDictionary["UIClick"][1]);
+        GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>().PlaySFX("UIClick", GameObject.FindWithTag("GameHandler").GetComponent<ReadSfxFile>().sfxDictionary["UIClick"][0], GameObject.FindWithTag("GameHandler").GetComponent<ReadSfxFile>().sfxDictionary["UIClick"][1]);
+        GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>().sfxSource.Pause();
         GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>().MusicVolume(0.25f);
         if (isAlmanac)
         {
@@ -36,7 +40,24 @@ public class PauseMenu : MonoBehaviour
         if (!prop)
             Time.timeScale = 1f;
         GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>().PlaySFX("UIClick", GameObject.FindWithTag("GameHandler").GetComponent<ReadSfxFile>().sfxDictionary["UIClick"][0], GameObject.FindWithTag("GameHandler").GetComponent<ReadSfxFile>().sfxDictionary["UIClick"][1]);
+        GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>().sfxSource.Play();
         GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>().MusicVolume(1f);
+    }
+
+    public void Home()
+    {
+        Time.timeScale = 1f;
+        // StartCoroutine(Camera.main.GetComponent<AudioManager>().FadeTwo(false, "BassyMain", "BassyEvent", 0f, 0f)); // Fade out two music
+        SceneManager.LoadScene("TitleScene");
+        GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>().StopMusic();
+    }
+    public void SfxControl()
+    {
+        GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>().SfxVolumeControl();
+    }
+    public void MusicControl()
+    {
+        GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>().MusicVolumeControl();
     }
 
 }
